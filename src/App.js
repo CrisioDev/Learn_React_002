3a580d2ae4832b5f2e7470c84f92d2ae8897ba40
CrisioDev <crisioletsplay@gmail.com>
2019-03-07 01:19:22 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserInput from './UserInput/UserInput'
+import UserOutput from './UserOutput/UserOutput'
 
 class App extends Component {
+  state = {
+    username: 'Crisio'
+  }
+
+  changeUsernameHandler = (event) =>{
+    this.setState({
+      username: event.target.value
+    });
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid #eee',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <UserInput style={style} name={this.state.username} changed={this.changeUsernameHandler}/>
+        <UserOutput name={this.state.username}/>
+        <UserOutput name="Gina"/>
+        <UserOutput name="Calvin"/>
       </div>
     );
   }
